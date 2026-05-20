f684cbdcf2673e716f7f18e888cc5f681216f085
maughan <rhys.maughan93@gmail.com>
2026-05-20 13:49:11 +0100

    - more modal fixes

--- app/components/modal/index.tsx
+++ app/components/modal/index.tsx
@@ -27,25 +27,22 @@ export default function Modal({
 
   return !open ? null : (
     <>
-      <div className="fixed inset-0 z-[998] bg-black/50" />
-
       <div
-        className="fixed inset-0 z-[999] flex items-center justify-center px-4"
+        className="fixed inset-0 z-[998] bg-black/50 flex items-center justify-center p-3"
         onClick={handleClose}
+      />
+      <div
+        className="max-h-[90%] z-[999] sm:w-[480px] overflow-auto rounded-[10px] fixed inset-0 m-auto mx-3 h-fit sm:mx-auto"
+        style={{ backgroundColor: "#142036", border: "1px solid #1E2C44" }}
       >
-        <div
-          className="max-h-[90%] w-full sm:w-[480px] overflow-auto relative rounded-[10px]"
-          style={{ backgroundColor: "#142036", border: "1px solid #1E2C44" }}
-        >
-          <div className="w-full relative">
-            {children}
+        <div className="w-full relative">
+          {children}
 
-            <X
-              size={16}
-              className="absolute top-3 right-3 cursor-pointer text-[#6E7A92] hover:text-[#E0E6F0] transition-colors"
-              onClick={handleClose}
-            />
-          </div>
+          <X
+            size={16}
+            className="absolute top-3 right-3 cursor-pointer text-[#6E7A92] hover:text-[#E0E6F0] transition-colors"
+            onClick={handleClose}
+          />
         </div>
       </div>
     </>
